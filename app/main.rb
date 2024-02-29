97ca127c99e98a64c86a093919384842b410af7c
Sam Markham <sam.markham@clio.com>
2024-02-29 09:45:22 -0800

    Revert "Project template"
    This reverts commit 022ec07339ec101f8996be10e9d6c2e3c38cf1fe.

--- app/main.rb
+++ app/main.rb
@@ -1,17 +1,19 @@
 require "app/scenes/intro"
 require "app/scenes/game"
+require "app/sprites/terrain"
 
 def tick args
   args.outputs.solids << [0, 0, 1280, 720, 0, 0, 0]
 
-  if args.state.scene.nil?
-    args.state.scene = :intro
-  end
+  Scenes::Game.tick(args)
+  # if args.state.scene.nil?
+  #   args.state.scene = :game
+  # end
 
-  case args.state.scene
-  when :intro
-    Scenes::Intro.tick(args)
-  when :game
-    Scenes::Game.tick(args)
-  end
+  # case args.state.scene
+  # when :intro
+  #   Scenes::Intro.tick(args)
+  # when :game
+  #   Scenes::Game.tick(args)
+  # end
 end
